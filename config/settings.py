4d99de09adb3177fd1e5fb3105d253ea08446c7c
MokshaTummaluru <112241855+mokshatummaluru@users.noreply.github.com>
2026-03-13 14:48:52 +0530

    Created views to register ,login and logout a user

--- config/settings.py
+++ config/settings.py
@@ -43,17 +43,18 @@ INSTALLED_APPS = [
     'corsheaders',
     'apps.authentication',
     'apps.query_engine',
+    'rest_framework_simplejwt.token_blacklist',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
